3d079d572910dbe5c7993cacf13a1cc76d118bc5
Jeff <salisbury.jeffery@gmail.com>
2020-08-22 13:08:45 -0400

    Creating a note taking app.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-card class="menu">
+    <v-toolbar flat color="primary" dark>
+      <!-- <v-toolbar-title>User Profile</v-toolbar-title> -->
+    </v-toolbar>
+    <v-tabs vertical>
+      <div v-for="item in tags" :key="item.id">
+        <v-tab>
+          {{ item.name }}
+        </v-tab>
+      </div>
+
+      <v-tab-item v-for="item in desc" :key="item.id">
+        <v-card flat>
+          <v-card-text>
+            <div v-for="(listItem, index) in item.list" :key="index">
+              Name: {{ listItem.name }}
+              <p>Location: {{ listItem.location }}</p>
+              <p>Info: {{ listItem.info }}</p>
+              <hr />
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: 'Menu',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+  data: () => ({
+    tags: [
+      { id: 1, name: 'Contacts' },
+      { id: 2, name: 'Locations' },
+      { id: 3, name: 'Loot' }
+    ],
+    desc: [
+      {
+        group: 'Contacts',
+        list: [
+          { name: 'Jeff', location: 'Earth', info: 'Does Programming' },
+          { name: 'Jeff', location: 'Earth', info: 'Does Programming' },
+          { name: 'Jeff', location: 'Earth', info: 'Does Programming' },
+          { name: 'Jeff', location: 'Earth', info: 'Does Programming' }
+        ]
+      },
+      { group: 'Locations', list: 'Something Something Darkside' },
+      {
+        group: 'Loot',
+        list:
+          'Cras sagittis. Phasellus nec sem in justo pellentesque facilisis.  Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.'
+      }
+    ]
+  })
+};
+</script>
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.menu {
+  padding: 30px;
 }
 </style>
